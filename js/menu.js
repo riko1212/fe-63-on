73aa17f3f3b213f95edf35515142293e70208aca
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2022-09-01 21:43:08 +0300

    mod-8

---
+++ js/menu.js
@@ -0,0 +1,42 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+
+//Закривається меню при переході на якір
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('[data-menu-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-close]'),
+//     menu: document.querySelector('[data-menu]'),
+//     body: document.querySelector('body'),
+//     menuList: document.querySelector('.mob-menu-list'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+//   refs.menuList.addEventListener('click', removeMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+
+//   function removeMenu() {
+//     refs.menu.classList.add('is-hidden');
+//     refs.body.classList.remove('no-scroll');
+//   }
+// })();
